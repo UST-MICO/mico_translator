fc21f747825408a5fe419d5002c13aa6a0a984ee
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2019-04-27 19:44:57 +0200

    Add CloudEvent class and implement json to xml translation

--- translator/translators.py
+++ translator/translators.py
@@ -1,16 +1,42 @@
+from .messages import CloudEvent
+from abc import abstractmethod
 from typing import Dict, Tuple
 
 
 class MessageTranslator():
+    """
+    Abstract message translator class.
+    """
 
     translators: Dict[Tuple[str, str], 'MessageTranslator'] = {}
 
     def __init_subclass__(cls, source_format='', target_format='', **kwargs):
+        """
+        Registration hook for subclasses.
+
+        All MessageTranslators need to provide source and target format.
+        """
         MessageTranslator.translators[(source_format, target_format)] = cls()
+        cls.source_format = source_format
+        cls.target_format = target_format
 
     @staticmethod
     def get_translator(source_format: str, target_format: str):
+        """
+        Get the translator translating from source to target format.
+        """
         return MessageTranslator.translators.get((source_format, target_format))
 
-    def translate(self, message: dict) -> dict:
+    @abstractmethod
+    def test_message(self, message: CloudEvent) -> bool:
+        """
+        Test wether the translator can handle the message.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def translate(self, message: CloudEvent) -> CloudEvent:
+        """
+        Actual translation method of the translator.
+        """
         raise NotImplementedError()
