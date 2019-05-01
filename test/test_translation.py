666a0c5fd20611333d929a67d2e36c2db73750e4
Pascal Weiß <pa-weiss@gmx.de>
2019-05-01 20:24:22 +0200

    Fix missing file error in TestMessageTranslator. Please use 'helpers.get_project_root()' if you want to open a file in the project (such as a config file). This is required since the working directory may vary on different machines, as it depends on the IDE settings, i.e. the interpreter settings

--- test/test_translation.py
+++ test/test_translation.py
@@ -1,15 +1,14 @@
-from pathlib import Path
-
 from json import load
 from translator.messages import CloudEvent
 from translator.translators import MessageTranslator
+from translator.helpers import get_project_root
 
 
 class TestMessageTranslator:
 
     def test_cloud_event_1(self):
         cloud_event: CloudEvent
-        with Path('./test/test_data/cloud_event_2.json').open() as _file:
+        with (get_project_root() / 'test' / 'test_data' / 'cloud_event_2.json').open() as _file:
             data = load(_file)
             cloud_event = CloudEvent(data)
         translator: MessageTranslator = MessageTranslator.get_translator('json', 'xml')
